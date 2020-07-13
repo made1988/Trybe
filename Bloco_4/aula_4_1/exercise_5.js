f98c97374c499a994c91639f752e5e2e0c975053
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-13 18:04:45 -0300

    exercicio 5, conferir se 3 angulos representam um triangulo

---
+++ Bloco_4/aula_4_1/exercise_5.js
@@ -0,0 +1,11 @@
+let alpha = 15;
+let beta = 55;
+let gamma = 120;
+let sum = (alpha + beta + gamma === 180)
+
+
+console.log(sum)
+
+if (!sum) {
+  console.log("Fatal error, these angles don't represent a triangle")
+}
