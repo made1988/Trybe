73e113819a82493da0ea38659190c6f7c47f83eb
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-14 18:47:06 -0300

    Bonus exercise 3, creating a new array multiplying each old value for the following value

--- Bloco_4/aula_4_2/bonus_ex_2.js
+++ Bloco_4/aula_4_2/bonus_ex_2.js
@@ -1,14 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let biggerNumbers = [];
 
-for (let i = 1; i < numbers.length; i++) {
-  for (let j = 0; j < i; j++) {
-    if (numbers[i] > numbers[j]) {
-      let position = numbers[i];
-  
-      numbers[i] = numbers[j];
-      numbers[j] = position;
-    }
+for (let i = 0; i < numbers.length; i += 1) {
+  let newNumber = 0;
+  if ( i < (numbers.length - 1)) {
+    newNumber = numbers[i] * numbers[i + 1];
+    biggerNumbers.push(newNumber);
+  } else {
+    newNumber = numbers[1] * 2;
   }
 }
 
-console.log(numbers);
+console.log(biggerNumbers);
