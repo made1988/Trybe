6f94b4e13812a1e3d9ab84584d527ad0ad9b1832
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-14 18:22:06 -0300

    Exercise 6, returning how many odd numbers are in an array

---
+++ Bloco_4/aula_4_2/exercise_6.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddNumbers = 0;
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 != 0) {
+    oddNumbers += 1;
+  }
+}
+
+console.log(oddNumbers);
