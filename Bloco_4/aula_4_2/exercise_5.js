e6bdcc715d8bcece8686e5e33597adfcde773d0a
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-15 15:40:38 -0300

    Correcting variable declaration inside first for loop

--- Bloco_4/aula_4_2/exercise_5.js
+++ Bloco_4/aula_4_2/exercise_5.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let compare = 0;
 
 for (let firstIndex = 0; firstIndex < numbers.length; firstIndex += 1) {
-  let compare = 0;
+  compare = 0;
   for (let secondIndex = 0; secondIndex < numbers.length; secondIndex += 1){
     if ( numbers[firstIndex] > numbers[secondIndex]) {
       compare += 1;
