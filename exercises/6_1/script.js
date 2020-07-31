45e432be7e275996353669c2090d5f797d55a2f5
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-31 19:17:33 -0300

    First fieldset done

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,5 +1,5 @@
-const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-const selectList = document.querySelector('#estado');
+const states = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+const selectList = document.querySelector('#state');
 
 function createOption() {
   const option = document.createElement('option');
@@ -17,5 +17,5 @@ function addOption(addTo, arrayToAdd) {
 }
 
 window.onload = function () {
-  addOption(selectList, estados);
+  addOption(selectList, states);
 };
