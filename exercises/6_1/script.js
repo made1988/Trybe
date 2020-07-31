a2a4aac060a4d1d54360095171b3596acccd92e7
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-07-31 19:03:15 -0300

    First commit (should have commited before) files and first fieldset done

---
+++ exercises/6_1/script.js
@@ -0,0 +1,21 @@
+const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+const selectList = document.querySelector('#estado');
+
+function createOption() {
+  const option = document.createElement('option');
+  return option;
+}
+
+function addOption(addTo, arrayToAdd) {
+  for (let i in arrayToAdd) {
+    let newOption = createOption();
+    newOption.value = i;
+    newOption.innerText = arrayToAdd[i];
+    addTo.appendChild(newOption);
+  }
+
+}
+
+window.onload = function () {
+  addOption(selectList, estados);
+};
