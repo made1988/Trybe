7e539935151b4def3ed64ed55bf8b9cacfa12e5d
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-11 18:03:37 -0300

    first exercise commit, for opening the branch

---
+++ exercises/7_2/exercise1.js
@@ -0,0 +1,25 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function addProperty(obj, key, value) {
+  obj[key] = value;
+}
+
+addProperty(lesson2, 'turno', 'manhã');
