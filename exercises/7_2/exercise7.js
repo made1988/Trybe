ea35a4c2e38fb9940b69743523af1640031535d4
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-11 18:54:35 -0300

    Exercise 7, getting a value from its position in the object

---
+++ exercises/7_2/exercise7.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+
+function getValueFromPosition(obj, pos) {
+  return Object.values(obj)[pos];
+}
+
+console.log(getValueFromPosition(lesson1, 0));
