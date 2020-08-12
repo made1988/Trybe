b06b612cc15b87afe7ec5459ece16c0bde1e0108
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-12 18:44:30 -0300

    First exercise without 4th item

--- exercises/7_3/exercise1.js
+++ exercises/7_3/exercise1.js
@@ -8,4 +8,6 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9, "Sum is not correct");
+assert.strictEqual(sum(0, 0), 0, "Sum is not correct");
+assert.strictEqual(sum(4, '5'), 9, "Sum is not correct");
