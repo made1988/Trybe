0b14477cb6911841f649d557c4737a96b642c920
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-19 18:44:46 -0300

    Fifth exercise, how many "A"s in an array of names

---
+++ exercises/8_3/exercise5.js
@@ -0,0 +1,23 @@
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  const howManyAs = names
+    .reduce((acc, name) => acc.concat(name))
+    .toUpperCase()
+    .split('')
+    .filter(item => item === 'A')
+    .length;
+  return howManyAs;
+}
+
+containsA();
+// assert.deepEqual(containsA(), 20);
