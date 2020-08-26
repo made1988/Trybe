42af4c72efd6755dd1a39582a07ca8e5ac4f4138
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-26 19:00:24 -0300

    Exercise 6, callback, delay and chance of error

--- exercises/9_1/exercise5.js
+++ exercises/9_1/exercise5.js
@@ -11,7 +11,7 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 
 // definição da função sendMarsTemperature...
 
-const sendMarsTemperature = (action) => action(getMarsTemperature());
+const sendMarsTemperature = (callback) => setTimeout(() => callback(getMarsTemperature()), messageDelay());
 
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
